495001ce2e4b14c13c1aee53d2522bd824e8fce0
tejendrapatel <info.prospectias@gmail.com>
2022-06-13 16:15:27 +0530

    first push

---
+++ zapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import authenticate,login,logout
+from zapp.models import *
+from django.shortcuts import render,redirect
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.core.mail import EmailMultiAlternatives
+from Govt.settings import EMAIL_HOST_USER
+from django.contrib.auth.models import User
+
+
+def Home(request):
+    return render(request, 'frontend/index.html')
